db765b7c9b721caa14a4cdecdddc1524cda3f828
TheCodeWorm <4purposeliving@gmail.com>
2018-05-04 08:16:44 -0700

    added dev tools to package.json and started setting up App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const puppeteer = require('puppeteer');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const isDebugging = () => {
+	const debugging_mode = {
+		headless: false, 
+		slowMo: 250, 
+		devtools: true
+	}
+	return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+}
+
+describe('on page load', () => {
+	test('h1 loads correctly', async () => {
+		let browser = await puppeteer.launch({})
+		let page = await browser.newPage()
+
+		page.emulat({
+			viewport: {
+				width: 500,
+				height: 2400
+			},
+			userAgent: ''
+		})
+})
+
